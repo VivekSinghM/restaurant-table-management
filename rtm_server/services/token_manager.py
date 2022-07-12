6233e926a4178e87e252c307b754f36f29b51daf
VivekSinghM <viveksinghmongare@gamil.com>
2022-07-13 01:01:38 +0530

    moved token related part to token_manager.py

--- rtm_server/services/token_manager.py
+++ rtm_server/services/token_manager.py
@@ -1,4 +1,13 @@
-token_key= jwt.encode({
+from datetime import datetime,timedelta
+import jwt
+
+from app import app
+
+class Token:
+    def get_token(public_id):
+        return jwt.encode({
             'public_id': public_id,
             'exp': datetime.utcnow() + timedelta(minutes = 30)
         }, app.secret_key)
+    def check_token(token):
+        pass
