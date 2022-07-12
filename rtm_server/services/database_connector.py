2b02b914dc59298df5c5d6b1668bd22b50bb1aed
VivekSinghM <viveksinghmongare@gamil.com>
2022-07-13 00:41:22 +0530

    added database_connector.py file to handle db con.

--- rtm_server/services/database_connector.py
+++ rtm_server/services/database_connector.py
@@ -1,6 +1,8 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 
+from constant import *
+
 app.config['SQLALCHEMY_DATABASE_URI']= SQL_URI[postgress]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
