04819c4aa50e908ff4b1060484407ef5236a0441
VivekSinghM <viveksinghmongare@gamil.com>
2022-08-01 22:15:26 +0530

    added mongodb

--- rtm_server/services/database_connector.py
+++ rtm_server/services/database_connector.py
@@ -7,3 +7,12 @@ app.config['SQLALCHEMY_DATABASE_URI']= SQL_URI[postgress]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
+
+from pymongo import MongoClient
+
+# Requires the PyMongo package.
+# https://api.mongodb.com/python/current
+
+mongo_client = MongoClient('mongodb://localhost:27017/')
+
+mongo_col = mongo_client['RTM']['Orders']
