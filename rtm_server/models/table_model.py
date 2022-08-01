71880dc7f23617a301e732b708d9d64dd72bbf5a
VivekSinghM <viveksinghmongare@gamil.com>
2022-08-01 22:18:49 +0530

    fixed Table model and added order method

--- rtm_server/models/table_model.py
+++ rtm_server/models/table_model.py
@@ -1,12 +1,12 @@
+from enum import unique
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import table
-from services.database_connector import db
+from services.database_connector import db, mongo_col
 
 
 class Table(db.Model):
-    table_id = db.Column(db.Integer, PrimaryKey=True, autoincrement=True)
+    table_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     table_no = db.Column(db.String(20), nullable=False, unique=True)
-    table_order = db.Column(db.String(10), db.ForeignKey('Order.id'), nullable=True)
+    table_order = db.Column(db.String(50), unique=True, nullable=True) 
     occupied = db.Column(db.Boolean, nullable=False)
     
     @staticmethod
@@ -17,17 +17,26 @@ class Table(db.Model):
     def add_table(table_no, table_order, occupied):
         try:
             db.session.add(table_no = table_no, table_order = table_order, occupied = occupied)
+            return True
         except Exception:
             return False
-        return True
-    
     @staticmethod
-    def update_occupied(id,occupied):
+    def update_occupied(id):
         try:
             Table.query.filter_by(table_id=id).first().occupied = not Table.query.filter_by(table_id=id).first().occupied
             return True
         except Exception:
             return False
     @staticmethod
-    def add_order():
-        pass
+    def add_order(id,order):
+        try:
+            order_id = str(mongo_col.insert_one(order).inserted_id)
+            print("Order Id:",order_id)
+            Table.query.filter_by(table_id=id).first().table_order= order_id
+            if (not Table.query.filter_by(table_id=id).first().occupied): Table.update_occupied(id)
+            db.session.commit()
+            return order_id
+        except Exception:
+            print("error occurred in adding order")
+            return False
+db.create_all()
