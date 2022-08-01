d43207f95f645289a639f1fd81b3e7362138021f
VivekSinghM <viveksinghmongare@gamil.com>
2022-08-01 22:13:42 +0530

    fixed route and added order_food()

--- rtm_server/api/table_manager.py
+++ rtm_server/api/table_manager.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import make_response, jsonify
+from flask import make_response, jsonify, request
 from models.table_model import Table
 
 from pyqrcode import create as create_qr
@@ -17,15 +17,20 @@ try:
 except Exception:
     pass
 
-@app.route('table/<str:t_id>')
-def load_table_data(t_id):
-    t_data = Table.get_Table().__dict__; t_data.pop()
-    return make_response( jsonify(Table.get_Table().__dict__) ,201)
+@app.route("/table", methods=["GET"])
+def load_table_data():
+    args = request.args
+    t_data = Table.get_Table(args.get("tId")).__dict__
+    return make_response( jsonify(t_data) ,201)
 
-@app.route('order/<str:t_id>', mtehod=['GET'])
+@app.route('/order', methods=['GET'])
 def order_food():
-    pass
+    data = request.args.to_dict()
+    print(data['tId'],data)
+    order_id=Table.add_order(data.pop('tId'),data)
+    return jsonify({'order_id':order_id})
 
-@app.route('bill/<str:t_id>', mtehod=['GET'])
+@app.route('/bill', methods=['GET'])
 def get_bill():
+    args = request.args
     pass
