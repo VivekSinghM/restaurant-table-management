6b57e7622e529bfbf381f5f96f08f941e4176ae0
VivekSinghM <viveksinghmongare@gamil.com>
2022-08-08 00:34:33 +0530

    added code to load menu data in table

--- rtm_server/services/token_manager.py
+++ rtm_server/services/token_manager.py
@@ -8,7 +8,7 @@ class Token:
     def get_token(public_id,exp_time):
         return jwt.encode({
             'public_id': public_id,
-            'exp': datetime.utcnow() + timedelta(minutes = 30)
+            'exp': datetime.utcnow() + timedelta(minutes = exp_time)
         }, app.secret_key)
     
     def check_token(token):
