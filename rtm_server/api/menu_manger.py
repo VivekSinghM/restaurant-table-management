f09d5b282f8c7f7393e0b62af89a3f80b187ab1b
VivekSinghM <viveksinghmongare@gamil.com>
2022-08-08 17:24:27 +0530

    added methos to get menu and additem to menu

---
+++ rtm_server/api/menu_manger.py
@@ -0,0 +1,16 @@
+import json
+from app import app
+from models.menu_model import Menu, TempMenu
+from flask import make_response, jsonify, request, send_file
+
+
+@app.route('/getMenu', methods=['GET'])
+def get_menu():
+    Menu.get_menu_dict()
+    return make_response(jsonify(TempMenu.menu) ,201)
+
+@app.route('/addMenuItems',methods=['GET'])
+def add_menu_item():
+    temp_items=json.load(request.data())
+    Menu.add_menu_item(temp_items)
+    return make_response('true',201)
