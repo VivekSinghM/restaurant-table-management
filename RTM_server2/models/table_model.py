a387f1a7ef36890ec6a3527c0a8a9ef137732d59
VivekSinghM <viveksinghmongare@gamil.com>
2022-08-12 04:43:54 +0530

    added new methos and fixed old

--- RTM_server2/models/table_model.py
+++ RTM_server2/models/table_model.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from services.database_connector import db, mongo_col
 from models.menu_model import Menu, TempMenu
+from sqlalchemy import null
 from bson.objectid import ObjectId
 
 
@@ -45,20 +46,22 @@ class Table(db.Model):
             return False
 
     @staticmethod
-    def add_order(id, order, uid=None):
+    def create_order(tId, order, uid=None):
         p_items = {}
         try:
-            
-            if TempMenu.menu=={}: Menu.get_menu_dict()
 
-            for [item_id, qty] in order.items():
-                p_items.update({TempMenu.menu[int(item_id)]['name']:qty})
+            if TempMenu.menu == {}:
+                Menu.get_menu_dict()
+            total = 0
+            for [item, qty] in order.items():
+                p_items.update({item: qty})
+                total=+qty*TempMenu.menu[item]['rate']
 
-            order_id = Order.set_order(p_items)
-            
-            Table.query.filter_by(table_id=id).first().table_order = order_id
-            if (not Table.query.filter_by(table_id=id).first().occupied):
-                Table.update_occupied(id)
+            order_id = Order.set_order(p_items, uid, total)
+            tempTable=Table.query.filter_by(table_id=tId).first()
+            tempTable.table_order = order_id
+            if (not tempTable.occupied):
+                Table.update_occupied(tId)
             db.session.commit()
             return order_id
         except Exception as e:
@@ -66,22 +69,36 @@ class Table(db.Model):
             return False
 
     @staticmethod
-    def add_items(id, order):
-        temp_order = {}
+    def update_order(oId, order):
+        temp_order = Order.get_order(oId)['order']
         try:
-            if TempMenu.menu=={}: Menu.get_menu_dict()
-            for [item_id, qty] in order.items():
-                item_name=TempMenu.menu[int(item_id)]['name']
-                temp_order.update({item_name:qty})
+            if TempMenu.menu == {}:
+                Menu.get_menu_dict()
+            for [item, qty] in order.items():
+                if qty < 0 : 
+                    raise Exception('nagetive qty is passed')
+                elif qty == 0 : 
+                    temp_order.pop(item,None) 
+                else :
+                    if item in temp_order : temp_order[item]+= qty
+                    else: temp_order[item] = qty
 
-            order_id = Order.update_order(id,temp_order)
-
-            print("data",mongo_col.find_one(id))
-
+            Order.update_order(id, temp_order)
             return True
         except Exception as e:
-            print("error occurred in adding items in order:",id)
+            print("error occurred in adding items in order:", id)
             return False
+    
+    @staticmethod
+    def close_order(tId,oId,paid):
+        tempTable=Table.query.filter_by(table_id=tId).first()
+        if Order.get_order(id=oId)['total']==paid :
+            tempTable.table_order = null
+            if (tempTable.occupied):
+                Table.update_occupied(tId)
+            return True
+        
+
 
     @staticmethod
     def get_tabels_dict():
@@ -89,11 +106,14 @@ class Table(db.Model):
 
     @property
     def serialize(self):
-        print(self.table_order==None)
+        order = {}
+        if self.table_order != None:
+            order = Order.get_order(self.table_order)
         return {
             self.table_id: {
                 "table_no": self.table_no,
                 "order_id": self.table_order,
+                "order": order,
                 "occupied": self.occupied,
             }
         }
@@ -101,42 +121,44 @@ class Table(db.Model):
 
 db.create_all()
 
+
 class Order:
     @staticmethod
     def get_order(id):
         try:
-            query={
+            query = {
                 '_id': ObjectId(id)
             }
             return mongo_col.find_one(filter=query)['order']
         except Exception as e:
-            print("erroe in loadin order for id:",id)
+            print("erroe in loadin order for id:", id)
 
     @staticmethod
     def get_orders(ids):
-        temp_orders={}
+        temp_orders = {}
         try:
             for id in ids:
-                query={
-                    '_id': ObjectId(id) 
+                query = {
+                    '_id': ObjectId(id)
                 }
-                items= mongo_col.find_one(filter=query)['order']
-                temp_orders[id]=items
-            print('order:',temp_orders)
+                items = mongo_col.find_one(filter=query)
+                temp_orders[id] = items
+            print('order:', temp_orders)
             return temp_orders
         except Exception as e:
-            print("erroe in loadin order for id:",id)
+            print("erroe in loadin order for id:", id)
             return False
-    
+
     @staticmethod
-    def set_order(p_items):
+    def set_order(p_items, uid, total):
         try:
             now = datetime.now()
             order_dict = {
-                'user': 'temp',
+                'user': uid,
                 'date': now.strftime("%m/%d/%Y"),
                 'time': now.strftime("%H:%M:%S"),
-                'order': p_items
+                'order': p_items,
+                "Paid": total
             }
 
             order_id = str(mongo_col.insert_one(order_dict).inserted_id)
@@ -146,25 +168,17 @@ class Order:
             print("error in adding order")
             return False
 
-
     @staticmethod
-    def update_order(id,items):
+    def update_order(id, items):
         try:
-            temp_order= Order.get_order(id)
-
-            print(temp_order,items)
-
-            for [key,val] in items.items():
-                if key in temp_order: temp_order[key]+=val
-                else: temp_order[key]=val
-            query={
+            query = {
                 '_id': ObjectId(id)
             }
-            data={'order':temp_order}
+            data = {'order': items}
             print(data)
-            return mongo_col.update_one(query, {'$set':data} , upsert=False).inserted_id
+            return mongo_col.update_one(query, {'$set': data}, upsert=False).inserted_id
         except Exception as e:
-            print("erroe in loadin order for id:",id)
+            print("erroe in loadin order for id:", id)
 
 
 # class CurrentOrders:
