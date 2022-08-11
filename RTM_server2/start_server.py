d36e12cd63f84de4a4626410da322d2d18b33fa5
VivekSinghM <viveksinghmongare@gamil.com>
2022-08-12 04:50:19 +0530

    idk

--- RTM_server2/start_server.py
+++ RTM_server2/start_server.py
@@ -1,8 +1,7 @@
+from models.menu_model import TempMenu
 from app import app
 import api.all_api_call
-import models.menu_model
-from models.table_model import Table
 
-Table.get_tabels_dict()
+TempMenu.load_All_Menu()
 if __name__ == '__main__':
     app.run(debug=True)
